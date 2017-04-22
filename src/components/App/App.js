1df07b0340b5bd707c89b2bfc6cbc75897345372
Andrei <Andrei_Hameza@epam.com>
2017-04-22 16:00:57 +0300

    Folders renaming

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import ticketsData from '../../utils/fakeData';
-import Tickets from '../tickets';
+import Tickets from '../Tickets';
 
 class App extends Component {
   render() {
