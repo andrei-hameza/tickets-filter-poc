97a731333148c862ec6032aa9e326f51d2b4ea8d
Andrei <Andrei_Hameza@epam.com>
2017-04-26 17:57:58 +0300

    Adds separate Logo component

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import cn from 'classnames';
+import './Logo.css';
+
+const Logo = ({ className, children, href = 'javascript:void(0)', ...other }) => {
+
+  return (
+    <a href={href} className={cn('clickable-logo', className)} {...other}>
+      { children }
+    </a>
+  );
+};
+
+export default Logo;
